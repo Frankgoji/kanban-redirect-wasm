943614a893ce2f134aa821f33f8a5a97148e55c1
Franklin Lee <frankgoji@gmail.com>
2024-02-07 02:11:24 -0800

    Restructure to allow UI to be displayed

--- react/app/kanban-redirect.js
+++ react/app/kanban-redirect.js
@@ -1,16 +1,22 @@
 import React, { useState, useEffect, useRef } from 'react';
 import ReactDOM from 'react-dom';
 
-import { handle_redirect } from './tumblr_kanban_rust.js';
+import { HandleRedirect } from './tumblr_kanban_rust.js';
 
 export const KanbanRedirect = (props) => {
+    const [op, setOp] = useState('Marking Post Done');
+    const [clearCount, setClearCount] = useState(0);
+
     useEffect(() => {
-        handle_redirect();
+        const hr = HandleRedirect.new();
+        hr.handle_redirect(setOp, setClearCount);
     }, []);
 
     return (
         <div className="root">
             <h1>Kanban Redirect</h1>
+            <h2>{op}...</h2>
+            {op === 'Clearing Done Posts' && <h3>Cleared Posts: {clearCount}</h3>}
         </div>
     );
 }
--- src/lib.rs
+++ src/lib.rs
@@ -4,7 +4,7 @@ use chrono_tz::US::Pacific;
 use reqwest::header::{AUTHORIZATION, CONTENT_TYPE};
 use serde::{Deserialize, Serialize};
 use serde_json::{json, Value};
-use wasm_bindgen::{JsCast, JsError, prelude::wasm_bindgen};
+use wasm_bindgen::{JsCast, JsError, JsValue, prelude::wasm_bindgen};
 use wasm_bindgen_futures::JsFuture;
 use web_sys::{FormData, RequestInit, Response, UrlSearchParams};
 
@@ -39,108 +39,101 @@ extern {
     fn alert(s: &str);
 }
 
-/// Handles redirect URL, parses state, calls appropriate handler
+/// Create a JS object to handle state of processing.
 #[wasm_bindgen]
-pub async fn handle_redirect() -> Result<(), JsError> {
-    let window = web_sys::window().ok_or_else(|| JsError::new("no global `window` exists"))?;
-    let document = window.document().ok_or_else(|| JsError::new("should have a document on window"))?;
-    let location = document.location().ok_or_else(|| JsError::new("couldn't get document.location"))?;
-    let search = location.search().or_else(|_| Err(JsError::new("failed to get query vars")))?;
-    let href = location.href().or_else(|_| Err(JsError::new("failed to get href")))?;
-    log(&format!("href: {href}"));
+pub struct HandleRedirect { }
 
-    let url_search = UrlSearchParams::new_with_str(&search).or_else(|_| Err(JsError::new("couldn't get params")))?;
-    let Some(code) = url_search.get("code") else {
-        let msg = format!("couldn't get code: {href}");
-        return Err(JsError::new(&msg));
-    };
-    let Some(state) = url_search.get("state") else {
-        let msg = format!("couldn't get state: {href}");
-        return Err(JsError::new(&msg));
-    };
-    log(&format!("code: {code}"));
-    log(&format!("state: {state}"));
+#[wasm_bindgen]
+impl HandleRedirect {
 
-    let decoded = BASE64_STANDARD.decode(state.as_bytes())?;
-    let decoded = String::from_utf8(decoded)?;
-    log(&format!("decoded state: {decoded}"));
-
-    let state: State = serde_json::from_str(&decoded)?;
-    let State { op, value } = state;
-
-    match op {
-        Op::Done => add_done_tag(code, value).await,
-        Op::Clear => clear_done_tags(code, value).await,
+    /// Constructor
+    pub fn new() -> HandleRedirect {
+        HandleRedirect { }
     }
-}
 
-/// Takes the state (token + postID) and finishes adding the tags
-async fn add_done_tag(code: String, post_id: String) -> Result<(), JsError> {
-    let token = get_token(code).await?;
-    log(&format!("token: {token}"));
+    /// Handles redirect URL, parses state, calls appropriate handler
+    /// Args: two JS callback functions:
+    /// set_op: sets string for which operation this is
+    /// set_clear_count: sets int for the number of posts cleared so far
+    pub async fn handle_redirect
+    (
+        &self,
+        set_op: &js_sys::Function,
+        set_clear_count: &js_sys::Function,
+    ) -> Result<(), JsError> {
+        let window = web_sys::window().ok_or_else(|| JsError::new("no global `window` exists"))?;
+        let document = window.document().ok_or_else(|| JsError::new("should have a document on window"))?;
+        let location = document.location().ok_or_else(|| JsError::new("couldn't get document.location"))?;
+        let search = location.search().or_else(|_| Err(JsError::new("failed to get query vars")))?;
+        let href = location.href().or_else(|_| Err(JsError::new("failed to get href")))?;
+        log(&format!("href: {href}"));
 
-    // get month
-    let dt = Utc::now();
-    let dt = dt.with_timezone(&Pacific);
-    let month = format!("{}", dt.format("%b"));
-    let tags = format!("kanban,done,{month}");
+        let url_search = UrlSearchParams::new_with_str(&search).or_else(|_| Err(JsError::new("couldn't get params")))?;
+        let Some(code) = url_search.get("code") else {
+            let msg = format!("couldn't get code: {href}");
+            return Err(JsError::new(&msg));
+        };
+        let Some(state) = url_search.get("state") else {
+            let msg = format!("couldn't get state: {href}");
+            return Err(JsError::new(&msg));
+        };
+        log(&format!("code: {code}"));
+        log(&format!("state: {state}"));
 
-    let client = reqwest::Client::new();
-    let post_url = format!("https://api.tumblr.com/v2/blog/frankgojikanban/posts/{post_id}");
-    let response = client.get(&post_url)
-        .header(AUTHORIZATION, format!("Bearer {token}"))
-        .send()
-        .await?;
-    if !response.status().is_success() {
-        let msg = format!("request failed, status: {:?}", response.status());
-        return Err(JsError::new(&msg));
+        let decoded = BASE64_STANDARD.decode(state.as_bytes())?;
+        let decoded = String::from_utf8(decoded)?;
+        log(&format!("decoded state: {decoded}"));
+
+        let state: State = serde_json::from_str(&decoded)?;
+        let State { op, value } = state;
+
+        // call set_op
+        let this = JsValue::null();
+        let op_str = match op {
+            Op::Done => "Marking Post Done",
+            Op::Clear => "Clearing Done Posts",
+        };
+        let _ = set_op.call1(&this, &JsValue::from(op_str));
+
+        match op {
+            Op::Done => HandleRedirect::add_done_tag(code, value).await,
+            Op::Clear => HandleRedirect::clear_done_tags(code, value, set_clear_count).await,
+        }
     }
-    let json: Value = response.json().await?;
-    log(&format!("Original post: {}", serde_json::to_string_pretty(&json)?));
 
-    let new_json: Value = json!({
-        "content": json["response"]["content"],
-        "tags": tags
-    });
-    log(&format!("Post Body: {}", serde_json::to_string_pretty(&new_json)?));
-    let response = client.put(&post_url)
-        .header(AUTHORIZATION, format!("Bearer {token}"))
-        .header(CONTENT_TYPE, "application/json")
-        .body(serde_json::to_string(&new_json)?)
-        .send()
-        .await?;
-    if !response.status().is_success() {
-        let msg = format!("request failed, status: {:?}", response.status());
-        return Err(JsError::new(&msg));
-    }
-    let json: Value = response.json().await?;
-    log(&format!("{}", serde_json::to_string_pretty(&json)?));
+    /// Takes the state (token + postID) and finishes adding the tags
+    async fn add_done_tag(code: String, post_id: String) -> Result<(), JsError> {
+        let token = HandleRedirect::get_token(code).await?;
+        log(&format!("token: {token}"));
 
-    let window = web_sys::window().ok_or_else(|| JsError::new("no global `window` exists"))?;
-    let document = window.document().ok_or_else(|| JsError::new("should have a document on window"))?;
-    let location = document.location().ok_or_else(|| JsError::new("couldn't get document.location"))?;
-    location.set_href(&format!("https://frankgojikanban.tumblr.com/post/{post_id}"))
-        .or_else(|_| Err(JsError::new("failed to set the location")))?;
+        // get month
+        let dt = Utc::now();
+        let dt = dt.with_timezone(&Pacific);
+        let month = format!("{}", dt.format("%b"));
+        let tags = format!("kanban,done,{month}");
 
-    Ok(())
-}
-
-/// Takes the state (token + month) and finishes clearing posts
-async fn clear_done_tags(code: String, month: String) -> Result<(), JsError> {
-    let token = get_token(code).await?;
-    log(&format!("token: {token}"));
-    let months = vec!["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
-    let month = month.parse::<usize>()? - 1;
-    let month = months[month];
-
-    let client = reqwest::Client::new();
-    let mut total = 1;
-
-    // since the limit per request is 20 posts, iterate after clearing this set
-    while total > 0 {
-        let posts_url = format!("https://api.tumblr.com/v2/blog/frankgojikanban/posts?tag[0]=done&tag[1]={month}&npf=true");
-        let response = client.get(&posts_url)
+        let client = reqwest::Client::new();
+        let post_url = format!("https://api.tumblr.com/v2/blog/frankgojikanban/posts/{post_id}");
+        let response = client.get(&post_url)
             .header(AUTHORIZATION, format!("Bearer {token}"))
+            .send()
+            .await?;
+        if !response.status().is_success() {
+            let msg = format!("request failed, status: {:?}", response.status());
+            return Err(JsError::new(&msg));
+        }
+        let json: Value = response.json().await?;
+        log(&format!("Original post: {}", serde_json::to_string_pretty(&json)?));
+
+        let new_json: Value = json!({
+            "content": json["response"]["content"],
+            "tags": tags
+        });
+        log(&format!("Post Body: {}", serde_json::to_string_pretty(&new_json)?));
+        let response = client.put(&post_url)
+            .header(AUTHORIZATION, format!("Bearer {token}"))
+            .header(CONTENT_TYPE, "application/json")
+            .body(serde_json::to_string(&new_json)?)
             .send()
             .await?;
         if !response.status().is_success() {
@@ -150,28 +143,37 @@ async fn clear_done_tags(code: String, month: String) -> Result<(), JsError> {
         let json: Value = response.json().await?;
         log(&format!("{}", serde_json::to_string_pretty(&json)?));
 
-        // total_posts is the number of posts, report it
-        let total_posts = json["response"]["total_posts"].as_i64()
-            .ok_or_else(|| JsError::new("could not get total_posts as an int"))?;
-        if total == 1 {
-            alert(&format!("Number of Done posts in {month}: {total_posts}"));
-        }
-        total = total_posts;
+        let window = web_sys::window().ok_or_else(|| JsError::new("no global `window` exists"))?;
+        let document = window.document().ok_or_else(|| JsError::new("should have a document on window"))?;
+        let location = document.location().ok_or_else(|| JsError::new("couldn't get document.location"))?;
+        location.set_href(&format!("https://frankgojikanban.tumblr.com/post/{post_id}"))
+            .or_else(|_| Err(JsError::new("failed to set the location")))?;
 
-        // iterate through each post and clear the tags
-        let posts = json["response"]["posts"].as_array().ok_or_else(|| JsError::new("failed to convert posts to array"))?;
-        for post in posts {
-            let new_json: Value = json!({
-                "content": post["content"],
-                "tags": ""
-            });
-            log(&format!("Post Body: {}", serde_json::to_string_pretty(&new_json)?));
-            let post_id = &post["id"];
-            let post_url = format!("https://api.tumblr.com/v2/blog/frankgojikanban/posts/{post_id}");
-            let response = client.put(&post_url)
+        Ok(())
+    }
+
+    /// Takes the state (token + month) and finishes clearing posts
+    async fn clear_done_tags
+    (
+        code: String,
+        month: String,
+        set_clear_count: &js_sys::Function,
+    ) -> Result<(), JsError> {
+        let token = HandleRedirect::get_token(code).await?;
+        log(&format!("token: {token}"));
+        let months = vec!["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
+        let month = month.parse::<usize>()? - 1;
+        let month = months[month];
+
+        let client = reqwest::Client::new();
+        let mut total = 1;
+        let mut i = 0;
+
+        // since the limit per request is 20 posts, iterate after clearing this set
+        while total > 0 {
+            let posts_url = format!("https://api.tumblr.com/v2/blog/frankgojikanban/posts?tag[0]=done&tag[1]={month}&npf=true");
+            let response = client.get(&posts_url)
                 .header(AUTHORIZATION, format!("Bearer {token}"))
-                .header(CONTENT_TYPE, "application/json")
-                .body(serde_json::to_string(&new_json)?)
                 .send()
                 .await?;
             if !response.status().is_success() {
@@ -180,43 +182,78 @@ async fn clear_done_tags(code: String, month: String) -> Result<(), JsError> {
             }
             let json: Value = response.json().await?;
             log(&format!("{}", serde_json::to_string_pretty(&json)?));
+
+            // total_posts is the number of posts, report it
+            let total_posts = json["response"]["total_posts"].as_i64()
+                .ok_or_else(|| JsError::new("could not get total_posts as an int"))?;
+            if total == 1 {
+                alert(&format!("Number of Done posts in {month}: {total_posts}"));
+            }
+            total = total_posts;
+
+            // iterate through each post and clear the tags
+            let posts = json["response"]["posts"].as_array().ok_or_else(|| JsError::new("failed to convert posts to array"))?;
+            for post in posts {
+                let new_json: Value = json!({
+                    "content": post["content"],
+                    "tags": ""
+                });
+                log(&format!("Post Body: {}", serde_json::to_string_pretty(&new_json)?));
+                let post_id = &post["id"];
+                let post_url = format!("https://api.tumblr.com/v2/blog/frankgojikanban/posts/{post_id}");
+                let response = client.put(&post_url)
+                    .header(AUTHORIZATION, format!("Bearer {token}"))
+                    .header(CONTENT_TYPE, "application/json")
+                    .body(serde_json::to_string(&new_json)?)
+                    .send()
+                    .await?;
+                if !response.status().is_success() {
+                    let msg = format!("request failed, status: {:?}", response.status());
+                    return Err(JsError::new(&msg));
+                }
+                let json: Value = response.json().await?;
+                log(&format!("{}", serde_json::to_string_pretty(&json)?));
+                i += 1;
+                let this = JsValue::null();
+                let _ = set_clear_count.call1(&this, &JsValue::from(i));
+            }
+            total -= posts.len() as i64;
         }
-        total -= posts.len() as i64;
+
+        let window = web_sys::window().ok_or_else(|| JsError::new("no global `window` exists"))?;
+        let document = window.document().ok_or_else(|| JsError::new("should have a document on window"))?;
+        let location = document.location().ok_or_else(|| JsError::new("couldn't get document.location"))?;
+        location.set_href("https://frankgojikanban.tumblr.com/tagged/done")
+            .or_else(|_| Err(JsError::new("failed to set the location")))?;
+
+        Ok(())
     }
 
-    let window = web_sys::window().ok_or_else(|| JsError::new("no global `window` exists"))?;
-    let document = window.document().ok_or_else(|| JsError::new("should have a document on window"))?;
-    let location = document.location().ok_or_else(|| JsError::new("couldn't get document.location"))?;
-    location.set_href("https://frankgojikanban.tumblr.com/tagged/done")
-        .or_else(|_| Err(JsError::new("failed to set the location")))?;
+    /// Can't use reqwest here due to multipart sending binary rather than text.
+    async fn get_token(code: String) -> Result<String, JsError> {
+        let api_key = "YOUR_API_KEY";
+        let secret_key = "YOUR_SECRET_KEY";
+        let window = web_sys::window().ok_or_else(|| JsError::new("no global `window` exists"))?;
 
-    Ok(())
-}
+        let form_data = FormData::new().or_else(|_| Err(JsError::new("couldn't create form data")))?;
+        form_data.set_with_str("grant_type", "authorization_code").or_else(|_| Err(JsError::new("couldn't set form data")))?;
+        form_data.set_with_str("code", &code).or_else(|_| Err(JsError::new("couldn't set form data")))?;
+        form_data.set_with_str("client_id", api_key).or_else(|_| Err(JsError::new("couldn't set form data")))?;
+        form_data.set_with_str("client_secret", secret_key).or_else(|_| Err(JsError::new("couldn't set form data")))?;
 
-/// Can't use reqwest here due to multipart sending binary rather than text.
-async fn get_token(code: String) -> Result<String, JsError> {
-    let api_key = "YOUR_API_KEY";
-    let secret_key = "YOUR_SECRET_KEY";
-    let window = web_sys::window().ok_or_else(|| JsError::new("no global `window` exists"))?;
+        let mut request_init = RequestInit::new();
+        request_init.method("POST")
+            .body(Some(&form_data));
+        let resp = JsFuture::from(window.fetch_with_str_and_init("https://api.tumblr.com/v2/oauth2/token", &request_init)).await
+            .or_else(|_| Err(JsError::new("failed to post to token")))?;
+        let resp: Response = resp.dyn_into().or_else(|_| Err(JsError::new("failed to get response")))?;
+        let json = JsFuture::from(resp.json().or_else(|_| Err(JsError::new("couldn't get response json")))?).await
+            .or_else(|_| Err(JsError::new("couldn't get response json")))?;
+        log(&format!("response: {:?}", json));
 
-    let form_data = FormData::new().or_else(|_| Err(JsError::new("couldn't create form data")))?;
-    form_data.set_with_str("grant_type", "authorization_code").or_else(|_| Err(JsError::new("couldn't set form data")))?;
-    form_data.set_with_str("code", &code).or_else(|_| Err(JsError::new("couldn't set form data")))?;
-    form_data.set_with_str("client_id", api_key).or_else(|_| Err(JsError::new("couldn't set form data")))?;
-    form_data.set_with_str("client_secret", secret_key).or_else(|_| Err(JsError::new("couldn't set form data")))?;
-
-    let mut request_init = RequestInit::new();
-    request_init.method("POST")
-        .body(Some(&form_data));
-    let resp = JsFuture::from(window.fetch_with_str_and_init("https://api.tumblr.com/v2/oauth2/token", &request_init)).await
-        .or_else(|_| Err(JsError::new("failed to post to token")))?;
-    let resp: Response = resp.dyn_into().or_else(|_| Err(JsError::new("failed to get response")))?;
-    let json = JsFuture::from(resp.json().or_else(|_| Err(JsError::new("couldn't get response json")))?).await
-        .or_else(|_| Err(JsError::new("couldn't get response json")))?;
-    log(&format!("response: {:?}", json));
-
-    let TokenResponse { access_token, .. } = serde_wasm_bindgen::from_value(json)?;
-    Ok(access_token)
+        let TokenResponse { access_token, .. } = serde_wasm_bindgen::from_value(json)?;
+        Ok(access_token)
+    }
 }
 
 #[cfg(test)]
